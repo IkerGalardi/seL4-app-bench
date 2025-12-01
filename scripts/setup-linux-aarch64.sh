6b7859a3145c5adb7a120471d72e53dbc735d3e5
IkerGalardi <contacto.ikergalardi@gmail.com>
2025-12-01 17:10:52 +0100

    meta: add script for downloading microkit SDK

---
+++ scripts/setup-linux-aarch64.sh
@@ -0,0 +1,4 @@
+wget https://github.com/seL4/microkit/releases/download/2.1.0/microkit-sdk-2.1.0-linux-aarch64.tar.gz
+tar -xzf microkit-sdk-2.1.0-linux-aarch64.tar.gz
+mv microkit-sdk-2.1.0 vendor/microkit-sdk
+rm microkit-sdk-2.1.0-linux-aarch64.tar.gz
