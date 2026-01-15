c2df41560e80a1ad579907c5d76dc75bff8716ff
IkerGalardi <contacto.ikergalardi@gmail.com>
2026-01-15 15:05:22 +0100

    webserver: implement userspace part of net stack

--- servers/webserver/entry.c
+++ servers/webserver/entry.c
@@ -11,6 +11,7 @@
 #include <sddf/network/lib_sddf_lwip.h>
 #include <sddf/timer/client.h>
 #include <sddf/timer/config.h>
+#include <lwip/pbuf.h>
 
 __attribute__((__section__(".serial_client_config")))
 serial_client_config_t serial_config;
@@ -28,6 +29,75 @@ serial_queue_handle_t serial_tx_queue_handle;
 
 net_queue_handle_t net_rx_handle;
 net_queue_handle_t net_tx_handle;
+
+#define LWIP_TICK_MS 100
+
+struct pbuf *head;
+struct pbuf *tail;
+
+static void netif_status_callback(char *ip_addr)
+{
+    sddf_printf("WEBSERVER: DHCP request finished, got IP %s\n", ip_addr);
+}
+
+static void set_timeout(void)
+{
+    sddf_timer_set_timeout(timer_config.driver_id, LWIP_TICK_MS * NS_IN_MS);
+}
+
+static net_sddf_err_t enqueue_pbufs(struct pbuf *p)
+{
+    /* Indicate to the tx virt that we wish to be notified about free tx buffers */
+    net_request_signal_free(&net_tx_handle);
+
+    if (head == NULL) {
+        head = p;
+    } else {
+        tail->next_chain = p;
+    }
+    tail = p;
+
+    /* Increment reference count to ensure this pbuf is not freed by lwip */
+    pbuf_ref(p);
+
+    return SDDF_LWIP_ERR_OK;
+}
+
+void transmit(void)
+{
+    bool reprocess = true;
+    while (reprocess) {
+        while (head != NULL && !net_queue_empty_free(&net_tx_handle)) {
+            net_sddf_err_t err = sddf_lwip_transmit_pbuf(head);
+            if (err == SDDF_LWIP_ERR_PBUF) {
+                sddf_dprintf("LWIP|ERROR: attempted to send a packet of size %u > BUFFER SIZE %u\n", head->tot_len,
+                             NET_BUFFER_SIZE);
+            } else if (err != SDDF_LWIP_ERR_OK) {
+                sddf_dprintf("LWIP|ERROR: unkown error when trying to send pbuf %p\n", head);
+            }
+
+            struct pbuf *temp = head;
+            head = temp->next_chain;
+            if (head == NULL) {
+                tail = NULL;
+            }
+            pbuf_free(temp);
+        }
+
+        /* Only request a signal if there are more pending pbufs to send */
+        if (head == NULL || !net_queue_empty_free(&net_tx_handle)) {
+            net_cancel_signal_free(&net_tx_handle);
+        } else {
+            net_request_signal_free(&net_tx_handle);
+        }
+        reprocess = false;
+
+        if (head != NULL && !net_queue_empty_free(&net_tx_handle)) {
+            net_cancel_signal_free(&net_tx_handle);
+            reprocess = true;
+        }
+    }
+}
 
 void init()
 {
@@ -47,9 +117,27 @@ void init()
                    net_config.tx.num_buffers);
     net_buffers_init(&net_tx_handle, 0);
 
+    sddf_lwip_init(&lwip_config, &net_config, &timer_config, net_rx_handle, net_tx_handle, NULL,
+                   netif_status_callback, enqueue_pbufs);
+    set_timeout();
+
+    sddf_lwip_maybe_notify();
+
     sddf_printf("WEBSERVER: initialized\n");
 }
 
 void notified(microkit_channel channel)
 {
+    if (channel == net_config.rx.id) {
+        sddf_printf("WEBSERVER: received notification from network (RX)\n");
+        sddf_lwip_process_rx();
+    } else if (channel == net_config.tx.id) {
+        sddf_printf("WEBSERVER: received notification from network (TX)\n");
+        transmit();
+    } else if (channel == timer_config.driver_id) {
+        sddf_lwip_process_timeout();
+        set_timeout();
+    }
+
+    sddf_lwip_maybe_notify();
 }
